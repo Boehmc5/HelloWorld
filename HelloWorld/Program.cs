026a12b80c11871d57d806365238e0e873a542f5
chris <chris@LAPTOP-T9OFFMSV>
2019-09-03 19:38:58 -0400

    <added comment>

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,6 +10,10 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            //Asks user to press any key to continue... then writes "Hello World".
+            Console.WriteLine("Press any key to continue....");
+            Console.WriteLine("Hello World");
+            Console.ReadKey(true);
         }
     }
 }
